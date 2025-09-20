e59b0008290ed79b895dbe575424c2daba159121
sunnysan-jpg <03sunnyyadav@gmail.com>
2025-09-20 20:09:47 +0530

    changes added for the back on top

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent { }
+export class FooterComponent {
+
+    scrollTop(e: Event) {
+  e.preventDefault();              // route change roko
+  if ('scrollBehavior' in document.documentElement.style) {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  } else {
+    window.scrollTo(0, 0);
+  }
+}
+ }
